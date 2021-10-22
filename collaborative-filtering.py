39cf62f0e5a09cb618e03e9c4ca5f1805cda2016
Sehyeon Kim <sue2368@gmail.com>
2021-10-22 17:07:57 +0900

    bug in making similar matrix

--- collaborative-filtering.py
+++ collaborative-filtering.py
@@ -16,8 +16,6 @@ for line in lines:
     mid_set.add(mid)
     ums.append((uid, mid, star))
 
-# print(min(uid_set))
-# print(min(mid_set))
 # make utility matrix M
 len_rows = max(uid_set)+1
 len_cols = max(mid_set)+1
@@ -25,21 +23,92 @@ M = np.zeros((len_rows, len_cols))
 for info in ums:
     uid, mid, star = info
     M[uid, mid] = star
-print (M.__repr__())
+# print (M.__repr__())
 
 # normalize M
 for irow in range(len_rows):
-    u_stars = M[irow,:]
-    nonzero_values = np.nonzero(u_stars)
+    u_stars = M[irow]
     if np.count_nonzero(u_stars) > 0:
-        avg_stars = u_stars[nonzero_values].mean()
-        # avg_stars = np.sum(u_stars)/np.count_nonzero(u_stars)
-        # print(np.sum(u_stars))
-        # print(len(nonzero_values))
-        # print (avg_stars)
-    # avg_stars = u_stars[np.nonzero(u_stars)].mean()
-    # avg_stars = np.nanmean(u_stars[u_stars!=0])
+        avg_stars = u_stars[np.nonzero(u_stars)].mean()
         for icol in range(len_cols):
             if (M[irow][icol]) != 0:
                 M[irow][icol] -= avg_stars
-print (M.__repr__())
+# print (M.__repr__())
+
+def cosine_distance(a, b):
+    mag_a = np.linalg.norm(a)
+    mag_b = np.linalg.norm(b)
+    ret = np.dot(a, b)/(mag_a*mag_b)
+    # print (ret)
+    return ret    
+
+# a = np.array([3,4])
+# b = np.array([6,8])
+# print(cosine_distance(a,b))
+
+# =====================================
+# user-based collaborative filtering
+# =====================================
+target_uid = 600
+dis_dic = {}
+target_stars = M[target_uid]
+# compute cosine distances between target user and all users
+for irow in range(len_rows):
+    if irow == target_uid:
+        continue
+    u_stars = M[irow]
+    if np.count_nonzero(u_stars) > 0:
+        cos_dis = cosine_distance(M[target_uid], u_stars)
+        if cos_dis not in dis_dic.keys():
+            dis_dic[cos_dis] = []
+        dis_dic[cos_dis].append(irow)
+# print (dis_dic)
+
+# find 10 most similar users
+top_ten_uid = []
+dis_list = dis_dic.keys()
+dis_list.sort()
+dis_list.reverse()
+while len(top_ten_uid) < 10:
+    max_dis = dis_list.pop(0)
+    sim_uids = dis_dic[max_dis]
+    for uid in sim_uids:
+        top_ten_uid.append(uid)
+print(top_ten_uid)
+
+# compute average ratings of I for similar users
+stars = {}
+sim_matrix = np.zeros((10,len_cols))
+i = 0
+for uid in top_ten_uid:
+    sim_matrix[i] = M[uid]
+    i+=1
+# print(sim_matrix.__repr__())
+
+for icol in range(len_cols):
+    m_stars = sim_matrix[:,icol]
+    if np.count_nonzero(m_stars) > 0:
+        avg_stars = m_stars[np.nonzero(m_stars)].mean()
+        if avg_stars not in stars.keys():
+            stars[avg_stars] = []
+        stars[avg_stars].append(icol)
+# print(stars)
+
+recommend = []
+star_list = stars.keys()
+star_list.sort()
+print(max(star_list))
+# star_list.reverse()
+# print(star_list)
+while len(recommend) < 5:
+    max_star = star_list.pop()
+    movies = stars[max_star]
+    print(max_star)
+    print(movies)
+    for mid in movies:
+        recommend.append(mid)
+print(recommend)
+
+# ===================================
+# item-based collaborative filtering
+# ===================================
