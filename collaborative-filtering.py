af2cf913b8f80bb347ee60d7456133fd5934b04e
Sehyeon Kim <sue2368@gmail.com>
2021-10-23 19:04:40 +0900

    final version

--- collaborative-filtering.py
+++ collaborative-filtering.py
@@ -21,12 +21,10 @@ for line in lines:
 # make utility matrix M
 len_rows = max(uid_set)+1
 len_cols = max(mid_set)+1
-# print("original len_cols = %f" %len_cols)
 M = np.zeros((len_rows, len_cols))
 for info in ums:
     uid, mid, star = info
     M[uid, mid] = star
-# print (M.__repr__())
 
 # normalize M
 for irow in range(len_rows):
@@ -38,14 +36,12 @@ for irow in range(len_rows):
         for icol in range(len_cols):
             if (M[irow][icol]) != 0:
                 M[irow][icol] -= avg_stars
-# print (M.__repr__())
 
 def cosine_distance(a, b):
     mag_a = np.linalg.norm(a)
     mag_b = np.linalg.norm(b)
     ret = np.dot(a, b)/(mag_a*mag_b)
-    # print (ret)
-    return ret    
+    return ret
 
 # =====================================
 # user-based collaborative filtering
@@ -72,13 +68,42 @@ rcmd_mids = np.argpartition(stars, -5)[-5:]
 rcmd_stars = stars[rcmd_mids] + target_avg
 
 # recommend 5 movies
-recommend = []
+user_base = []
 for i in range(5):
-    recommend.append((rcmd_mids[i], rcmd_stars[i]))
-recommend.sort(key=lambda x: -x[1])
-for ele in recommend:
+    user_base.append((rcmd_mids[i], rcmd_stars[i]))
+user_base.sort(key=lambda x: -x[1])
+for ele in user_base:
     print("%d\t%f" %(ele[0], ele[1]))
 
 # ===================================
 # item-based collaborative filtering
 # ===================================
+# find 10 similar movies to each movie
+# and compute average ratings
+avg_ratings = np.zeros(1001)
+for icol in range(1001): # for movie 1 to 1000
+    i_stars = M[:,icol]
+    if np.count_nonzero(i_stars) <= 0: # if movie doesn't exist
+        continue
+    m_distances = np.zeros(len_cols) # store distances between icol movie and all other movies
+    for jcol in range(len_cols):
+        j_stars = M[:,jcol]
+        if np.count_nonzero(j_stars) <= 0:
+            continue
+        cos_dis = cosine_distance(i_stars, j_stars)
+        m_distances[jcol] = cos_dis
+    sim_mids = np.argpartition(m_distances, -10)[-10:] # 10 similar movies
+    tuser_stars = M[target_uid,sim_mids] # ratings by target user for 10 similar movies
+    if np.count_nonzero(tuser_stars) > 0: # if target user rates at least one movie among them
+        avg_star = tuser_stars[np.nonzero(tuser_stars)].mean() # take average the ratings
+        avg_ratings[icol] = avg_star
+        top20_movies = np.argpartition(avg_ratings, -20)[-20:]
+
+# recommend 5 movies
+item_base = []
+for mid in top20_movies:
+    item_base.append((mid, avg_ratings[mid] + target_avg))
+item_base.sort(key=lambda x: (-x[1],x[0]))
+for i in range(5):
+    ele = item_base[i]
+    print("%d\t%f" %(ele[0], ele[1]))
