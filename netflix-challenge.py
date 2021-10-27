a7d0de98490c91fada429bf6bb9c444a2374f2cc
Sehyeon Kim <sue2368@gmail.com>
2021-10-27 19:57:00 +0900

    implement uv decomposition

--- netflix-challenge.py
+++ netflix-challenge.py
@@ -4,12 +4,17 @@ import numpy as np
 file = sys.argv[1]
 f = open(file, 'r')
 lines = f.readlines()
+test_file = sys.argv[2]
+tf = open(test_file, 'r')
+lines_tf = tf.readlines()
 target_uid = 600
 target_avg = 0
 
 uid_set = set()
 mid_set = set()
 ums = []
+pred_info = [] # store information to be predicted
+
 # data preprocessing
 for line in lines:
     info = line.split(',')
@@ -17,6 +22,12 @@ for line in lines:
     uid_set.add(uid)
     mid_set.add(mid)
     ums.append((uid, mid, star))
+for line in lines_tf:
+    info = line.split(',')
+    pred_info.append(info)
+    uid, mid = (int(info[0]), int(info[1]))
+    uid_set.add(uid)
+    mid_set.add(mid)
 
 # make utility matrix M
 len_rows = max(uid_set)+1
@@ -25,85 +36,127 @@ M = np.zeros((len_rows, len_cols))
 for info in ums:
     uid, mid, star = info
     M[uid, mid] = star
+original_M = np.copy(M)
 
-# normalize M
+avg_users = np.zeros(len_rows) # store average stars of each user
+avg_items = np.zeros(len_cols) # store average stars of each movie
+
+# get average stars of movies
+for icol in range(len_cols):
+    m_stars = np.copy(M[:,icol])
+    if np.count_nonzero(m_stars) <= 0:
+        continue
+    nonzero_index = np.nonzero(m_stars)
+    avg_stars = m_stars[nonzero_index].mean()
+    avg_items[icol] = avg_stars
+
+# get average stars of users
 for irow in range(len_rows):
     u_stars = np.copy(M[irow])
-    if np.count_nonzero(u_stars) > 0:
-        nonzero_index = np.nonzero(u_stars)
-        avg_stars = u_stars[nonzero_index].mean()
-        if irow == target_uid:
-            target_avg = avg_stars
-        u_stars[nonzero_index] -= avg_stars
-        M[irow] = u_stars
-
-def cosine_distance(a, b):
-    mag_a = np.linalg.norm(a)
-    mag_b = np.linalg.norm(b)
-    # ret = np.dot(a, b)/(mag_a*mag_b)
-    ret = np.matmul(a, np.transpose(b))/(mag_a*mag_b)
-    return ret
-
-# =====================================
-# user-based collaborative filtering
-# =====================================
-# find 10 most similar users
-distances = np.zeros(len_rows)
-target_stars = M[target_uid]
-for irow in range(len_rows):
-    u_stars = M[irow]
-    if np.count_nonzero(u_stars) > 0:
-        cos_dis = cosine_distance(target_stars, u_stars)
-        distances[irow] = cos_dis
-sim_uids = np.argpartition(distances, -10)[-10:]
-
-# compute average ratings of I for similar users
-stars = np.zeros(1001)
-for icol in range(1001):
-    m_stars = np.copy(M[sim_uids,icol])
-    if np.count_nonzero(m_stars) > 0:
-        avg_stars = m_stars[np.nonzero(m_stars)].mean()
-        stars[icol] = avg_stars
-rcmd_mids = np.argpartition(stars, -5)[-5:]
-rcmd_stars = stars[rcmd_mids] + target_avg
-
-# recommend 5 movies
-user_base = []
-for i in range(5):
-    user_base.append((rcmd_mids[i], rcmd_stars[i]))
-user_base.sort(key=lambda x: -x[1])
-for ele in user_base:
-    print("%d\t%f" %(ele[0], ele[1]))
-
-# ===================================
-# item-based collaborative filtering
-# ===================================
-# find 10 similar movies to each movie
-# and compute average ratings
-avg_ratings = np.zeros(1001)
-for icol in range(1001): # for movie 1 to 1000
-    i_stars = np.copy(M[:,icol])
-    if np.count_nonzero(i_stars) <= 0: # if movie doesn't exist
+    if np.count_nonzero(u_stars) <= 0:
         continue
-    m_distances = np.zeros(len_cols) # store distances between icol movie and all other movies
-    for jcol in range(len_cols):
-        j_stars = np.copy(M[:,jcol])
-        if np.count_nonzero(j_stars) <= 0:
-            continue
-        cos_dis = cosine_distance(i_stars, j_stars)
-        m_distances[jcol] = cos_dis
-    sim_mids = np.argpartition(m_distances, -10)[-10:] # 10 similar movies
-    tuser_stars = M[target_uid,sim_mids] # ratings by target user for 10 similar movies
-    if np.count_nonzero(tuser_stars) > 0: # if target user rates at least one movie among them
-        avg_star = tuser_stars[np.nonzero(tuser_stars)].mean() # take average the ratings
-        avg_ratings[icol] = avg_star
-        top20_movies = np.argpartition(avg_ratings, -20)[-20:]
+    nonzero_index = np.nonzero(u_stars)
+    avg_stars = u_stars[nonzero_index].mean()
+    avg_users[irow] = avg_stars
+    if irow == target_uid:
+        target_avg = avg_stars
+    # normalize M
+    # by subracting average of average stars of item and user
+    u_stars[nonzero_index] -= (avg_stars + avg_items[nonzero_index])/2
+    M[irow] = u_stars
 
-# recommend 5 movies
-item_base = []
-for mid in top20_movies:
-    item_base.append((mid, avg_ratings[mid] + target_avg))
-item_base.sort(key=lambda x: (-x[1],x[0]))
-for i in range(5):
-    ele = item_base[i]
-    print("%d\t%f" %(ele[0], ele[1]))
+# UV decomposition
+# repeat UV decomposition and take the average of the results
+start, last, interval = 1,2,1
+reps = (last-start)/interval
+r = 37 # total number of genres
+final_U = np.zeros((len_rows, r))
+final_V = np.zeros((r, len_cols))
+perturb = np.arange(start,last, interval) # uniform perturbation
+
+# order the optimization of the elements of U and V
+permutation = []
+for i in range(len_rows):
+    for j in range(r):
+        permutation.append((0,i,j))
+for i in range(r):
+    for j in range(len_cols):
+        permutation.append((1,i,j))
+np.random.shuffle(permutation)
+
+# perform the optimization
+for p in perturb:
+    print("im doing")
+    # initialize U and V with small perturbation
+    U = np.zeros((len_rows, r)) + p
+    V = np.zeros((r, len_cols)) + p
+    for perm in permutation:
+        # print("im working")
+        if perm[0] == 0: # optimize u_ix
+            i = perm[1]
+            x = perm[2]
+            print (i)
+            numer = 0
+            v_x = np.copy(V[x])
+            m_i = np.copy(M[i])
+            u_i = np.copy(U[i])
+            nz_col = np.where(m_i != 0)[0]
+            if len(nz_col) <= 0:
+                continue
+            for col in nz_col:
+                for k in range(r):
+                    v_k = np.copy(V[k])
+                    if k == x:
+                        continue
+                    numer += v_x[col] * (m_i[col] - u_i[k]*v_k[col])
+            denom = np.sum(np.square(v_x[nz_col]))
+            opt_u = numer/denom
+            U[i][x] = opt_u
+        else: # optimize v_xj
+            x = perm[1]
+            j = perm[2]
+            numer = 0
+            u_x = np.copy(U[:,x])
+            m_j = np.copy(M[:,j])
+            v_j = np.copy(V[:,j])
+            nz_row = np.where(m_j != 0)[0]
+            if len(nz_row) <= 0:
+                continue
+            for row in nz_row:
+                for k in range(r):
+                    u_k = np.copy(U[:,k])
+                    if k == x:
+                        continue
+                    numer += u_x[row] * (m_j[row] - u_k[row]*v_j[k])
+            denom = np.sum(np.square(u_x[nz_row]))
+            opt_v = numer/denom
+            V[x][j] = opt_v
+    final_U += U
+    final_V += V
+
+# to avoid overfitting
+final_U = final_U/reps
+final_V = final_V/reps
+final_UV = np.dot(final_U, final_V)
+
+# write result with predicted star
+g = open("output.txt", 'w')
+for info in pred_info:
+    uid = int(info[0])
+    mid = int(info[1])
+    add = (avg_users[uid] + avg_items[mid])/2
+    pred_star = final_UV[uid][mid] + add
+    info[2] = str(pred_star)
+    g.write(",".join(info))
+
+## test result
+result = np.zeros((len_rows, len_cols))
+for row in range(len_rows):
+    for col in range(len_cols):
+        result[row][col] = final_UV[row][col] + (avg_users[uid] + avg_items[mid])/2
+# print(final_U)
+# print(final_V)
+# print(final_UV)
+# print(M)
+# print(result)
+# print(original_M)
